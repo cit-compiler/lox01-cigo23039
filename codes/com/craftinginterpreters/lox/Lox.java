dbb0b897efdb9ef89562208e4aa353ce6b68d755
cigo23039 <cigo23039@g.nihon-u.ac.jp>
2026-01-13 09:48:13 +0900

    First commit

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -5,18 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+// import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
     public static void main(String[] args) throws IOException{
-        if(args.length>1){
+        if(args.length > 1){
             System.out.println("Usage: jlox [script]");
-            System.exit(64); 
-        } else if (args.length == 1) {
+            System.exit(64);
+        }else if(args.length == 1){
             runFile(args[0]);
-        } else {
+        }else{
             runPrompt();
         }
     }
@@ -25,41 +26,48 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
 
-        if (hadError) System.exit(65);
+        if(hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException{
-        InputStreamReader  input = new  InputStreamReader ( System.in ) ;
-        BufferedReader reader = new BufferedReader ( input ); 
-        
-        for (;;) {
-            System.out.print ( " > " ) ; String
-            line = reader.readLine ( ) ;
-            if ( line == null ) break ;
-            run ( line ) ;
-             
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) { 
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line); 
             hadError = false;
         }
     }
 
     private static void run(String source){
-        Scanner scanner=new Scanner(source);
+        Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        if(hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
-
     static void error(int line, String message) {
         report(line, "", message);
     }
 
-    private static void report(int line, String where, String message) {
+    private static void report(int line, String where,
+                          String message) {
         System.err.println(
             "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
     }
 
+    static void error(Token token, String message){
+        if(token.type == TokenType.EOF){
+            report(token.line,"at end", message);
+        }else {
+            report(token.line,"at '"+ token.lexeme + "'", message);
+        }
+    }
 }
-
--- codes/com/craftinginterpreters/lox/Token.java
+++ codes/com/craftinginterpreters/lox/Token.java
@@ -1,12 +1,12 @@
 package codes.com.craftinginterpreters.lox;
 
 public class Token {
-    final TokenType type;
-    final String lexeme;
-    final Object literal;
-    final int line; 
+  final TokenType type;
+  final String lexeme;
+  final Object literal;
+  final int line; 
 
-    Token(TokenType type, String lexeme, Object literal, int line) {
+  Token(TokenType type, String lexeme, Object literal, int line) {
     this.type = type;
     this.lexeme = lexeme;
     this.literal = literal;
@@ -16,5 +16,4 @@ public class Token {
   public String toString() {
     return type + " " + lexeme + " " + literal;
   }
-  
 }
